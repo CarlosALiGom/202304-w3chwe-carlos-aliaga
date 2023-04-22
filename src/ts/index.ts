d472575f685b919952ccad3e69c370464b297a88
Carlos Aliaga <carlosaligom@gmai.com>
2023-04-22 20:32:05 +0200

    Create PokemonListComponent Component

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,8 +1,10 @@
 import AppComponent from "./components/AppComponent/AppComponent.js";
 import PokemonCardComponent from "./components/PokemonCardComponent/PokemonCard.js";
+import PokemonListComponent from "./components/PokemonListComponent/PokemonListComponent.js";
 import { type PokemonStructure } from "./components/types";
 
 const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 const divContainer: HTMLElement = document.querySelector(".root")!;
 
 new AppComponent(divContainer);
+new PokemonListComponent(divContainer);
