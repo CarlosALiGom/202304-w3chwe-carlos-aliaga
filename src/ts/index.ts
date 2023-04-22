477fa11a34820de82827ebf013274d2a5291a7b4
Carlos Aliaga <carlosaligom@gmai.com>
2023-04-22 18:38:24 +0200

    Install isomorphic fetch to test PokemonCardComponent

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -6,12 +6,3 @@ const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 const divContainer: HTMLElement = document.querySelector(".root")!;
 
 new AppComponent(divContainer);
-
-const getPokemons = async (): Promise<PokemonStructure> => {
-  const response = await fetch(`${apiUrl}1`);
-  const pokemon = (await response.json()) as PokemonStructure;
-  new PokemonCardComponent(divContainer, pokemon);
-  return pokemon;
-};
-
-await getPokemons();
